74764e46c8a402c4f2549aedadd8ced512da1380
pattyjonsson <patriciajonsson@live.se>
2016-05-10 13:15:59 +0200

    changes in PWM testing
    Added InitPIN40 and InitPIN38 in test for PWM

--- Task1501c-master/ProjectShield/ProjectShield/test/test_PWM.c
+++ Task1501c-master/ProjectShield/ProjectShield/test/test_PWM.c
@@ -13,6 +13,8 @@ uint32_t *const p_PMC_PCSR1 = (uint32_t *) 0x400E0708U;
 
 void testPWMInitialisesProperly(void){
 	PWMInit();
+	InitPIN40();
+	InitPIN38();
 	TEST_ASSERT_BIT_HIGH_MESSAGE(4, *p_PMC_PCSR1, "PMC has not enabled the peripheral PWM clock");	/* Asserting the PMC manager has activated the PWM clock */
 	TEST_ASSERT_BIT_HIGH_MESSAGE(3, *p_PWM_CHSR, "PWM channel 3 is not enabled");	/* Asserting that PWM channel 3 is enabled */
 	TEST_ASSERT_BIT_HIGH_MESSAGE(2, *p_PWM_CHSR, "PWM channel 2 is not enabled");	/* Asserting that PWM channel 2 is enabled */
